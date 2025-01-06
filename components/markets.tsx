c494afb9b773480733f2927d6c05196707e8d734
YongheeZF <yongheechannra@gmail.com>
2025-01-06 21:00:38 +0700

    update

--- components/markets.tsx
+++ components/markets.tsx
@@ -78,7 +78,7 @@ export default function Markets() {
       },
         {
           title: "Floating Text & Leaderboard Pro Max (No Resource Pack)",
-          image: "/images/leaderboard1_2",
+          image: "/images/leaderboard1_3",
           imageType: "png",
           description: "อักษรลอยรองรับการพิมพ์ข้อความยาวๆหลายบรรทัด และ กระดานผู้นำที่กำหนดการแสดงผลได้สุดเฟี้ยว",
           link: "http://tiny.cc/AmethystCore",
@@ -94,7 +94,7 @@ export default function Markets() {
         },
         {
           title: "Private Safe House",
-          image: "/images/vault1_2",
+          image: "/images/vault1_3",
           imageType: "png",
           description: "บ้าน หรือ ห้องเก็บของส่วนตัวใครๆก็ทำได้ใช่ไหมล่ะ แต่จะมีซักเท่าไหร่ที่จะไม่บัคและไม่โดนเกรียน?",
           link: "http://tiny.cc/AmethystCore" 
